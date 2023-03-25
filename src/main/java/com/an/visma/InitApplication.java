717a82d8765446150dd8c5e34a594883af87ecf1
Aurimas <>
2023-03-25 11:35:53 +0200

    parsing data file content; calculated total revenue

--- src/main/java/com/an/visma/InitApplication.java
+++ src/main/java/com/an/visma/InitApplication.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class InitApplication {
 
     public static void main(String[] args) throws IOException {
-        VismaApp app = new VismaApp();
         String content = Utils.readFileToString("data.csv");
+        VismaApp app = new VismaApp(content);
     }
 
 }
--- src/main/java/com/an/visma/VismaApp.java
+++ src/main/java/com/an/visma/VismaApp.java
@@ -1,5 +1,43 @@
 package com.an.visma;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class VismaApp {
 
+    private final List<Transaction> transactions;
+
+    public VismaApp(String csvData) {
+        transactions = Stream.of(csvData.split(System.lineSeparator()))
+                .map(line -> line.split(","))
+                .map(row -> {
+                    try {
+                        return new Transaction(
+                                row[0], // transaction id
+                                row[1], // customer id
+                                row[2], // item id
+                                new SimpleDateFormat("yyyy-MM-dd").parse(row[3]), // transaction date
+                                Double.parseDouble(row[4]), // item price
+                                Integer.parseInt(row[5]) // item quantity
+                        );
+                    } catch (ParseException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .toList();
+
+        var totalRevenue = transactions.stream()
+                .mapToDouble(t -> t.itemPrice() * t.itemQuantity())
+                .sum();
+        System.out.println("Total revenue:" +totalRevenue);
+    }
+
+}
+
+record Transaction(String id, String customerId, String itemId, Date date, Double itemPrice, Integer itemQuantity) {
 }
